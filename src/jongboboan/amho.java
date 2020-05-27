27df159575afed2966827320e2f38d97eb045851
연우 이 <lyw9114@gmail.com>
2020-05-27 12:19:45 +0900

    중복제거중

--- src/jongboboan/amho.java
+++ src/jongboboan/amho.java
@@ -7,17 +7,27 @@ public class amho {
 		Scanner sc = new Scanner(System.in);
 		String inputS = new String();
 		char input[] = new char[25];
+		char temp[] = new char[25];
 		//스트링으로 입력 받기
 		inputS = sc.next();
 		
-		//입력받은 문자열 중복제거
-		
-		
-		//스트링을 char로 바꾸기
 		for(int i=0; i<inputS.length(); i++) {
-			input[i]=inputS.charAt(i);
+			temp[i]=inputS.charAt(i);
 		}
-		
+		//스트링을 char로 바꾸기, 중복제거
+		for(int i=0; i<inputS.length(); i++) {
+			for(int j=1;j<inputS.length();j++) {
+				if(temp[i] == inputS.charAt(j)) {
+					continue;
+				}
+				System.out.print("input[i]:"+input[i]+"  inputS.charAt(j):"+inputS.charAt(j));
+				System.out.println();
+				input[i]=inputS.charAt(j);
+			}
+		}
+		for(int i=0; i<input.length; i++) {
+			System.out.print(input[i]);
+		}
 		
 		//입력받은 문자를 암호판에 넣었을 경우 행과열 구하기
 		int col = (inputS.length())/5;

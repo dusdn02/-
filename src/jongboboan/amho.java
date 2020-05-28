bb399fc73e238917dcf66fb706c0701678880348
연우 이 <lyw9114@gmail.com>
2020-05-28 21:53:53 +0900

    프레임에 암호화, 복호화 선택 버튼

--- src/jongboboan/amho.java
+++ src/jongboboan/amho.java
@@ -1,12 +1,57 @@
 package jongboboan;
 
+import java.awt.Dimension;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class amho {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class amho extends JFrame{
+	
 	
 	public static char alphabetBoard[][] = new char[5][5];
 	
+	public amho() {
+		setLayout(null);
+		Panel p = new Panel();
+		
+		JButton am = new JButton("암호화");
+		JButton bok = new JButton("복호화");
+		am.setBounds(25, 100,200,150);
+		bok.setBounds(300, 100,200,150);
+		
+		am.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//암호화 패널?프레임 열기
+			}
+		});
+		bok.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//복호화 패널?프레임 열기
+			}
+		});
+		
+		add(am);
+		add(bok);
+		
+		add(p);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(550, 400);
+	    setVisible(true);
+	}
+	
 	public static void main(String[] args) {
+		
+		new amho();
+		
 		Scanner sc = new Scanner(System.in);
 //		System.out.print("암호화할 문자열을 입력하세요 : ");
 //		String str =  sc.nextLine();	
@@ -64,5 +109,7 @@ public class amho {
 						
 		
 	}
+	
 
 }
+
